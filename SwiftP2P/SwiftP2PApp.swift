da1ea2d7179ef142d3e4c1d9441897d40db4916a
shunsuke-tamura <wwwxx4869@gmail.com>
2024-09-07 15:42:01 +0900

    Initial Commit

---
+++ SwiftP2P/SwiftP2PApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftP2PApp.swift
+//  SwiftP2P
+//
+//  Created by shunsuke tamura on 2024/09/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftP2PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
